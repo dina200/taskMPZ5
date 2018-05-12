15e0e42f1cde54a0cee5213196e9e9f9d5ee483b
dina200 <dinapeace@ya.ru>
2018-05-12 17:04:40 +0300

    Third commit

--- taskUniver.cpp
+++ taskUniver.cpp
@@ -1,69 +1,102 @@
 #include "simc.h"
-#include <iostream>
 
-using namespace std;
-
-enum colors{
+enum details{
 	Green,
-	Red
+	Red, 
+	Fail
 };
 
 int main() {
-	cout << Green;
-	cout << Red;
-//	int t1 = 3, t2 = 5, t3 = 18;
-//	
-//	initlist(100);
-//	pfacility sver_stanok, fraiz_stanok, tokar_stanok;
-//	pqueue green, redf, redt;
-//	newfac(sver_stanok, "sver_stanok"); 
-//	newfac(fraiz_stanok, "fraiz_stanok");
-//	newfac(tokar_stanok, "tokar_stanok");
-//	newqueue(green, "Queue Green to sver_stanok");
-//    newqueue(redf, "Queue Red to fraiz_stanok");
-//    newqueue(redt, "Queue Red to tokar_stanok");
-//	initcreate(1,45);// detail
-//
-//	initcreate(20,210);// fail
-//	starttrace();
-//	
-//	 while(systime < 8 * 60) {	 	
-//	 	plan();	 		
-//        switch(sysevent) {
-//            case 1: create(1); break;
-//            case 2: if(randab(0, 1, v1) < 0,2){
-//            			trans->pi[0] = Green;
-//            			inqueue(green);
-//					} 
-//					else{
-//						trans->pi[0] = Red;	
-//					}inqueue(green); break;
-//            case 3: seize(sver_stanok); break;
-//            case 4: outqueue(green); break;
-//            case 5: delayt(t1); break;
-//            case 6: outfac(sver_stanok); break;
-//            case 7: destroy(); break;
-//            
-//            
-//            case 8: create(5); break;
-//            case 9: inqueue(redf); break;
-//            case 10: seize(fraiz_stanok); break;
-//            case 11: outqueue(redf); break;
-//            case 12: delayt(t2); break;
-//            case 13: outfac(fraiz_stanok); break;
-//            case 14: inqueue(redt); break;            
-//            case 15: seize(tokar_stanok); break;
-//            case 16: outqueue(redt); break;
-//            case 17: delayt(t3); break;  
-//			case 18: outfac(tokar_stanok); break;          
-//            case 19: destroy(); break;
-//            case 20: infac(tokar_stanok); break;
-//            case 21: delayt(45); break;
-//            case 22: outfac(tokar_stanok);break;
-//            case 23: destroy();break;
-//            		
-//        }
-//    }
-//	printall();
+	
+	int ttok = 18;
+	
+	initlist(8 * 60);
+	
+	pfacility sver_stanok, fraiz_stanok, tokar_stanok;
+	
+	pqueue qs, qf, qt;
+	newfac(sver_stanok, "sver_stanok"); 
+	newfac(fraiz_stanok, "fraiz_stanok");
+	newfac(tokar_stanok, "tokar_stanok");
+	newqueue(qs, "Queue Green to sver_stanok");
+    newqueue(qf, "Queue Red to fraiz_stanok");
+    newqueue(qt, "Queue Red to tokar_stanok");
+	
+	initcreate(1, 45);// detail	
+	
+	starttrace();
+	
+	 while(systime < 8 * 60) {	 	
+	 	plan();	 		
+        switch(sysevent) {
+            case 1: create(1); break;            
+			case 2: if(randab(0, 1, v1) < 0,2){
+            			trans->pi[0] = Green;
+            			inqueue(qs);
+					} 
+					else{
+						trans->pi[0] = Red;
+						trans->pr[0] = 22; 
+						inqueue(qf);	
+					} 
+					break;            
+			case 3: if(trans->pi[0] == Green) seize(sver_stanok);
+					else if(trans->pi[0] == Red) seize(fraiz_stanok); 	
+			 		break;            
+			case 4: if(trans->pi[0] == Green) outqueue(qs);
+					else if(trans->pi[0] == Red) outqueue(qf); 	
+			 		break;			
+            case 5: if(trans->pi[0] == Green) { 
+            			trans->pr[0] = 2.0;
+						delayt(randab(2, 4, v1));
+					}
+					else if(trans->pi[0] == Red) { 
+						if (randab(0, 1, v1) <= 0,15) trans->pr[1] = 25;
+						else trans->pr[1] = randab(6, 20, v1);
+						delayt(randab(4, 6, v1));						
+					} break;
+            
+			case 6: if(trans->pi[0] == Green) outfac(sver_stanok);
+					else if(trans->pi[0] == Red) outfac(fraiz_stanok); 	
+			 		break; 
+						
+            case 7: if(trans->pi[0] == Green) destroy(); 
+            		else if((trans->pi[0] == Red) && (trans->pr[0] + trans->pr[1]) > 45) delayt(randab(1, 3, v1));  
+					else if(trans->pi[0] == Red) next(8);         		          
+					break;
+					
+			case 8: if(trans->pi[0] == Red) inqueue(qt); 
+					break;
+            case 9: if(trans->pi[0] == Red) seize(tokar_stanok); 
+            		break;            		
+            case 10: if(trans->pi[0] == Red) outqueue(qt);
+            		break;            
+			case 11: if(trans->pi[0] == Red) delayt(ttok); 
+            		break;					
+			case 12: if(trans->pi[0] == Red) outfac(tokar_stanok);
+            		break;			
+			
+			case 13: if(randab(0, 1, v1) < 0.075) {
+					trans->pb[0] = true;
+					} break;
+			
+			case 14: if(trans->pb[0]) {
+					infac(tokar_stanok);
+					} break;
+			case 15: if(trans->pb[0]) {
+					delayt(randab(75, 165, v1));
+					} break;
+			case 16: if(trans->pb[0]) {
+					outfac(tokar_stanok);
+					} break;
+			
+			case 17: if(trans->pb[0]) {
+					trans->pb[0] = false;
+					destroy();					
+					} break;
+			case 18: if(trans->pi[0] == Red) destroy(); break;
+        }
+    }
+	printall();
 	return 0;
 }
